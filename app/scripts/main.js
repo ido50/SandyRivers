433c2bdbee663c7735f94b726cc5d5ea34754942
Ido Perlmuter <ido@ido50.net>
2013-09-02 00:27:33 +0300

    added jwerty and initial keyboard commands

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,28 @@ $(function() {
 
 	ko.applyBindings(vm);
 
+	jwerty.key('x', function() {
+		var current = vm.current_entry();
+		if (current < vm.entries().length) {
+			vm.mark_as_read(current);
+			vm.current_entry(current + 1);
+		}
+	});
+
+	jwerty.key('z', function() {
+		var current = vm.current_entry();
+		if (current >= -1) {
+			if (current >= 0) vm.mark_as_read(current);
+			vm.current_entry(current - 1);
+		}
+	});
+
+	jwerty.key('c', function() {
+		var current = vm.current_entry();
+		if (current >= 0 && current < vm.entries().length)
+			window.open(vm.entries()[current]._id, '_blank');
+	});
+
 	vm.load_entries();
 });
 
@@ -13,7 +35,7 @@ function viewModel() {
 
 	self.entries = ko.observableArray([]);
 
-	self.current_entry = ko.observable('');
+	self.current_entry = ko.observable(-1);
 
 	self.load_entries = function() {
 		console.log("Loading entries");
@@ -26,12 +48,16 @@ function viewModel() {
 		});
 	};
 
-	self.toggle_entry = function(entry) {
-		if (self.current_entry() == entry._id) {
-			self.current_entry('');
-			self.entries.remove(entry);
+	self.mark_as_read = function(index) {
+		//whatever
+	};
+
+	self.toggle_entry = function(index, event, entry) {
+		if (self.current_entry() == index) {
+			self.current_entry(-1);
+			//self.entries.splice(index, 1);
 		} else {
-			self.current_entry(entry._id);
+			self.current_entry(index);
 		}
 	};
 
